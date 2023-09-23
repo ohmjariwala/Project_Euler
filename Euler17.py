6e212ce4fc648c060c5b71745758fcbc338d0d10
ohmjariwala <145733466+ohmjariwala@users.noreply.github.com>
2023-09-23 15:03:44 -0400

    Delete Euler17.py

--- Euler17.py
+++
@@ -1,6 +0,0 @@
-#If all the numbers from 1 to 1000 (one thousand) inclusive were written out in words, how many letters would be used
-#Do not count space or hyphens
-
-sum=0
-for i in range (1,1000):
-    
